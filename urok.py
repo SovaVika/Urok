3b0c51fa8d4007dc7ce8797c62929cb149d09855
Sova Vika <sovavika1106@gmail.com>
2024-03-16 11:46:21 +0200

    urok

--- urok.py
+++ urok.py
@@ -1,11 +1,25 @@
 class Student:
     print("Hello")
+    def __init__ (self, name, money, height):
+        self.name = name
+        self.height = height
+        self.money = money
+        print("Init")
+    def BuyPizza(self):
+        self.money -= 100
+        print("Now you have" , self.money, "money and pizza")
+
+class Car:
     def __init__ (self):
-        self.height = 160
-        self.money = 100
+        self.color = "red"
+        self.model = "Bugatti"
         print("Init")
 
-s = Student()
-s2 = Student()
+s = Student("Alex", 200, 190)
+s2 = Student("Kate", 300, 170)
+print(s.name,s.money,s2.name,s2.money)
 s.money = 200
+car = Car()
+print(car.color,car.model)
 print(s.money, s2.money)
+s.BuyPizza()
